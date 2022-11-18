4aa4c122707e2b91c795d36ae2c1a7a77220f9a7
heejin <raccoonboy0803@gmail.com>
2022-11-18 21:22:56 +0900

    app.js 충돌 해제

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import JobSearch from './components/JobSearch/JobSearch';
-import Filter from './components/JobSearch/Filter';
+
 import { Router } from 'react-router-dom';
 function App() {
   return (
