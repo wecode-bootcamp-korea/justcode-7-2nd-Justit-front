5901f5700eaa7c81f5d1d149b7152ce665e83737
Sungah Park <sonia.0903.park@gmail.com>
2022-11-18 21:20:13 +0900

    11월 18일 금 오후 9시 20분

--- src/pages/Router.js
+++ src/pages/Router.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
-import Home from './Home/Home';
 import MyPage from './MyPage/MyPage';
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
         <Route path="/MyPage" element={<MyPage />} />
       </Routes>
     </BrowserRouter>
