c0eb68b267538965875d88dcca319ee17bf6a551
kiminSong <kiminsee22@gmail.com>
2022-11-23 15:45:30 +0900

    router 수정

--- src/pages/Router.js
+++ src/pages/Router.js
@@ -3,19 +3,17 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import MyPage from './MyPage/MyPage';
 import Detail from './Detail/Detail';
 import Company from './Company/Company';
-import Main from './Main/Main';
 import JobSearch from './JobSearch/JobSearch';
 import Home from './Home/Home';
 import Resume from './Resume/Resume';
 import Footer from '../components/Footer/Footer';
-import Signup from '../components/Signup/Signup';
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/MyPage" element={<MyPage />} />
-        <Route path="/" element={<Main />} />
+        <Route path="/" element={<Home />} />
         <Route path="/posts" element={<JobSearch />} />
         <Route path="/resume" element={<Resume />} />
         <Route path="/footer" element={<Footer />} />
@@ -23,7 +21,6 @@ function Router() {
         <Route path="/signup" element={<Signup />} /> */}
         <Route path="/detail" element={<Detail />} />
         <Route path="/company" element={<Company />} />
-        <Route path="/main" element={<Main />} />
       </Routes>
     </BrowserRouter>
   );
